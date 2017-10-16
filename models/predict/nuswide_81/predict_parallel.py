895191b6374ce3121ede024f7e29a721fe74f96d
caozhangjie <caozhangjie14@gmail.com>
2017-10-16 14:08:29 +0800

    fix predict bug

--- models/predict/nuswide_81/predict_parallel.py
+++ models/predict/nuswide_81/predict_parallel.py
@@ -28,19 +28,20 @@ def mean_average_precision(params):
     sim = np.dot(database_code, validation_code.T)
     ids = np.argsort(-sim, axis=0)
     APx = []
-    
+    ground_truth = np.sum(np.dot(database_labels, validation_labels.T), axis=0)
+    validation_labels[validation_labels==0] = -1
     for i in range(query_num):
-        label = validation_labels[i, :]
-        label[label == 0] = -1
-        idx = ids[:, i]
-        imatch = np.sum(database_labels[idx[0:R], :] == label, axis=1) > 0
-        relevant_num = np.sum(imatch)
-        Lx = np.cumsum(imatch)
-        Px = Lx.astype(float) / np.arange(1, R+1, 1)
-        if relevant_num != 0:
-            APx.append(np.sum(Px * imatch) / relevant_num)
-        else:
-            APx.append(np.sum(Px * imatch) / 1)            
+        label = validation_labels[i, :]        
+        if ground_truth[i] > 0:
+            idx = ids[:, i]
+            imatch = np.sum(database_labels[idx[0:R], :] == label, axis=1) > 0
+            relevant_num = np.sum(imatch)
+            Lx = np.cumsum(imatch)
+            Px = Lx.astype(float) / np.arange(1, R+1, 1)
+            if relevant_num != 0:
+                APx.append(np.sum(Px * imatch) / relevant_num)
+            else:
+                APx.append(np.sum(Px * imatch) / 1)            
     
     return np.mean(np.array(APx))
         
@@ -105,20 +106,31 @@ def get_codes_and_labels(params):
     return dict(database_code=np.sign(np.array(database_code)), database_labels=np.array(database_labels), validation_code=np.sign(np.array(validation_code)), validation_labels=np.array(validation_labels))
 
 
-parser = argparse.ArgumentParser(description='Predict Hash Code.')
-parser.add_argument('--gpu', metavar='N', type=str, nargs='?', default="0",
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Predict Hash Code.')
+    parser.add_argument('--gpu', metavar='N', type=str, nargs='?', default="0",
                     help='running gpu id')
-parser.add_argument('--model_path', type=str, nargs=1,                    
+    parser.add_argument('--model_path', type=str, nargs="?",                    
                     help='the caffemodel path')
-parser.add_argument('--save_path', type=str, nargs=1,                    
+    parser.add_argument('--save_path', type=str, nargs='?',                    
                     help='the path to save predict code')
+    parser.add_argument('--code_path', type=str, nargs='?', default="",
+                    help='the predicted hash code path')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-nthreads = 8
-params = []
-for i in range(nthreads):
-    params.append(dict(model_file="./models/predict/deploy.prototxt",
+
+    if args.code_path:
+        code_and_label = {}
+        code_and_label["database_code"] = np.load(opath.join(args.code_path, "database_code.npy"))
+        code_and_label['database_labels'] = np.load(opath.join(args.code_path,"database_label.npy"))
+        code_and_label["validation_code"] = np.load(opath.join(args.code_path, "validation_code.npy"))
+        code_and_label['validation_labels'] = np.load(opath.join(args.code_path, "validation_label.npy"))
+    else:
+        nthreads = 8
+        params = []
+        for i in range(nthreads):
+            params.append(dict(model_file="./models/predict/deploy.prototxt",
                   pretrained_model=args.model_path[0],
                   image_dims=(256,256),
                   scale=255,
@@ -128,23 +140,22 @@ for i in range(nthreads):
                   mean_file="./python/caffe/imagenet/ilsvrc_2012_mean.npy",
                   gpu=args.gpu[0]))
 
-pool = Pool(nthreads)
-results = pool.map(get_codes_and_labels, params)
-print("start combine")
-code_and_label = results[0]
-for i in range(1, nthreads):
-    code_and_label['database_code'] = np.vstack([code_and_label["database_code"], results[i]['database_code']])
-    code_and_label['database_labels'] = np.vstack([code_and_label["database_labels"],results[i]['database_labels']])
-    code_and_label['validation_code'] = np.vstack([code_and_label["validation_code"], results[i]['validation_code']])
-    code_and_label['validation_labels'] = np.vstack([code_and_label["validation_labels"], results[i]['validation_labels']])
+        pool = Pool(nthreads)
+        results = pool.map(get_codes_and_labels, params)
+        print("start combine")
+        code_and_label = results[0]
+        for i in range(1, nthreads):
+            code_and_label['database_code'] = np.vstack([code_and_label["database_code"], results[i]['database_code']])
+            code_and_label['database_labels'] = np.vstack([code_and_label["database_labels"],results[i]['database_labels']])
+            code_and_label['validation_code'] = np.vstack([code_and_label["validation_code"], results[i]['validation_code']])
+            code_and_label['validation_labels'] = np.vstack([code_and_label["validation_labels"], results[i]['validation_labels']])
+
+        print("saving ...")
+        code_and_label['path'] = str(args.save_path[0])
+        save_code_and_label(code_and_label)
+        print("saving done")
 
 print("start predict")
 code_and_label['R'] = 5000
 mAP = mean_average_precision(code_and_label)
-
 print ("MAP: "+ str(mAP))
-print("saving ...")
-code_and_label['path'] = str(args.save_path[0])
-save_code_and_label(code_and_label)
-print("saving done")
-
