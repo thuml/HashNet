9d87b6fc1c3f724670e888cb2ec21aad7d58f3c2
Zhangjie Cao <caozhangjie14@gmail.com>
2019-08-02 10:32:09 -0700

    Update loss.py

--- pytorch/src/loss.py
+++ pytorch/src/loss.py
@@ -21,3 +21,24 @@ def pairwise_loss(outputs1, outputs2, label1, label2, sigmoid_param=1.0, l_thres
     loss = (torch.sum(torch.masked_select(exp_loss, Variable(mask_ep))) + torch.sum(torch.masked_select(dot_loss, Variable(mask_dp)))) * class_num + torch.sum(torch.masked_select(exp_loss, Variable(mask_en))) + torch.sum(torch.masked_select(dot_loss, Variable(mask_dn)))
 
     return loss / (torch.sum(mask_positive.float()) * class_num + torch.sum(mask_negative.float()))
+
+def pairwise_loss_updated(outputs1,outputs2,label1,label2):
+    similarity = Variable(torch.mm(label1.data.float(), label2.data.float().t()) > 0).float()
+    dot_product = torch.mm(outputs1, outputs2.t())
+    #exp_product = torch.exp(dot_product)
+
+    mask_positive = similarity.data > 0
+    mask_negative = similarity.data <= 0
+    exp_loss = torch.log(1+torch.exp(-torch.abs(dot_product))) + torch.max(dot_product, Variable(torch.FloatTensor([0.]).cuda()))-similarity * dot_product
+    #weight
+    S1 = torch.sum(mask_positive.float())
+    S0 = torch.sum(mask_negative.float())
+    S = S0+S1
+    exp_loss[similarity.data > 0] = exp_loss[similarity.data > 0] * (S / S1)
+    exp_loss[similarity.data <= 0] = exp_loss[similarity.data <= 0] * (S / S0)
+
+    loss = torch.sum(exp_loss) / S
+
+    #exp_loss = torch.sum(torch.log(1 + exp_product) - similarity * dot_product)
+
+    return loss
